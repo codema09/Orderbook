075b2160856f0ce75a35c5fe6080b5316272e400
KHR09 <hrushikeshkalikiri@kgpian.iitkgp.ac.in>
2025-08-18 20:34:25 +0600

    Init commit

---
+++ build/CMakeFiles/OrderBookLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrderBookLib.dir/src/OrderBook.cpp.o"
+  "CMakeFiles/OrderBookLib.dir/src/OrderBook.cpp.o.d"
+  "libOrderBookLib.a"
+  "libOrderBookLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrderBookLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OrderbookTest.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrderbookTest.dir/link.d"
+  "CMakeFiles/OrderbookTest.dir/OrderbookTest/pch.cpp.o"
+  "CMakeFiles/OrderbookTest.dir/OrderbookTest/pch.cpp.o.d"
+  "CMakeFiles/OrderbookTest.dir/OrderbookTest/test.cpp.o"
+  "CMakeFiles/OrderbookTest.dir/OrderbookTest/test.cpp.o.d"
+  "OrderbookTest"
+  "OrderbookTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrderbookTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
